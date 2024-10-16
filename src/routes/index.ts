362a57864aedd91fdc43728ff7d9af697d983a88
Khel Tan <kheltan.engineering@gmail.com>
2024-10-16 21:44:28 +0200

    Get algorithm from remote Mongodb database by slug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { algorithmExecutor } from "src/controllers/algorithm/algorithm-executor";
 import { router as algorithmExecuteRouter } from "./algorithm.execute.route";
+import { router as databaseRouter } from "./database.algorithms.route";
 import express from "express"
-import { run } from "../services/database";
 
 const router = express.Router();
 
-router.get("/database", run);
+router.use("/algorithms", databaseRouter);
 router.use("/algorithm/execute", algorithmExecuteRouter);
 
 export { router };
--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -7,9 +7,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    let connection = await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    let db = connection.db("syga");
+    let collection = db.collection("algorithms");
+    let results = await collection.find({})
+      .limit(50)
+      .toArray();
+    return results;
+
   }
   finally {
     await client.close();
@@ -17,4 +24,23 @@ async function run() {
 
 }
 
-export { run };
+async function getAlgorithm(identifier: string | number) {
+  if (typeof identifier == "number") {
+    throw new Error("Algorithm retrieval by id not implemented yet");
+  }
+  try {
+    let connection = await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    let db = connection.db("syga");
+    let collection = db.collection("algorithms");
+    let query = { name: identifier };
+    let result = await collection.findOne(query);
+    return result;
+  }
+  finally {
+    await client.close();
+  }
+}
+
+export { getAlgorithm };
