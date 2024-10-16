7b1b8ddfc82ff1ef5b051a3a68ab641286c99a6c
Khel Tan <kheltan.engineering@gmail.com>
2024-10-16 22:58:43 +0200

    Connection to Python backend container

---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  ENGINE_IMAGE: "syga-backend",
+}
--- src/services/algorithm-runner/algorithm-runner.ts
+++ src/services/algorithm-runner/algorithm-runner.ts
@@ -1,9 +1,25 @@
 import { VisualizationResult } from "src/shared/VisualizationResult";
 import { simpleGraph } from "../../../test/mock/testFrames";
+import { environment } from "../../environment";
+
+import { exec } from "child_process";
+
+const { ENGINE_IMAGE } = environment;
+
 
 class AlgorithmRunner {
+  get engineShellCommand() {
+    return `docker container run --rm -i ${ENGINE_IMAGE} < temp/temp.json`
+  }
   async run(): Promise<VisualizationResult> {
-    console.log("Running algorithm");
+
+    exec(this.engineShellCommand, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    })
     return {
       frames: simpleGraph,
     };
