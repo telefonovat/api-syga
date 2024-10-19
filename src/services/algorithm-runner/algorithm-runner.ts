7914c366ced55fdfaa6d40bb04e1671166acb743
Khel Tan <kheltan.engineering@gmail.com>
2024-10-20 00:28:02 +0200

    Adapt legacy backend data formats to new formats

--- src/services/algorithm-runner/algorithm-runner.ts
+++ src/services/algorithm-runner/algorithm-runner.ts
@@ -1,13 +1,45 @@
 import { VisualizationResult } from "src/shared/VisualizationResult";
 import { simpleGraph } from "../../../test/mock/testFrames";
 
-class AlgorithmRunner {
-  async run(): Promise<VisualizationResult> {
-    console.log("Running algorithm");
-    return {
-      frames: simpleGraph,
-    };
+import childProcess from "child_process";
+import { backendAdaptor, LegacyVisualizationResult } from "../adaptors/backend-adaptor";
+const safeJsonParse = <T>(str: string) => {
+  try {
+    const jsonValue: T = JSON.parse(str);
+
+    return jsonValue;
+  } catch {
+    return undefined;
   }
+};
+const exec = (command: string, options = {}) =>
+  new Promise<{ stdout: string, stderr: string }>((resolve, reject) =>
+    childProcess.exec(command, options, (error, stdout, stderr) =>
+      error ? reject(error) : resolve({ stdout, stderr })
+    )
+  );
+interface Algorithm {
+  code: string,
+}
+class AlgorithmRunner {
+  async run(algorithm: Algorithm): Promise<VisualizationResult> {
+    const { stdout, stderr } = await exec(this.shellCommand, {});
+
+
+    const legacyOutput = safeJsonParse<LegacyVisualizationResult>(stdout);
+    if (legacyOutput !== undefined) {
+
+      return backendAdaptor.convertToVisualizationResult(legacyOutput);
+    }
+    else {
+      throw new Error("Backend sent gibberish");
+    }
+  }
+  get shellCommand() {
+    return "docker container run --rm -i syga-backend < ./src/services/algorithm-runner/test.json";
+  }
+
+
 }
 
-export { AlgorithmRunner };
+export { AlgorithmRunner, Algorithm };
--- src/shared/VisualizationResult.ts
+++ src/shared/VisualizationResult.ts
@@ -1,4 +1,11 @@
 import { Frame } from "./Frame"
 export interface VisualizationResult {
+  timestamp: string,
+  response: string,
+  error?: string,
+  algTime: number,
+  parseTime: number,
+  elapsed: number,
+
   frames: Frame[],
 }
