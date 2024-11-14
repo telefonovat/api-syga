6e25b6b2b9da796c2a2257a4663f80304d9fd7dd
Khel Tan <kheltan.engineering@gmail.com>
2024-11-14 15:54:59 +0100

    Change engine image name

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
   PORT: process.env.API_PORT || 6000,
-  ENGINE_IMAGE: process.env.ENGINE_IMAGE || 'engine',
+  ENGINE_IMAGE: process.env.ENGINE_IMAGE || 'syga/engine',
 };
 
 export { config };
