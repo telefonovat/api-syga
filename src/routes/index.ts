a3478cc77c27f8c25e423fc241c9f77cc7cbaf49
Khel Tan <kheltan.engineering@gmail.com>
2025-01-14 17:50:23 +0100

    Refactor : Algorithm execution route moved

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,12 @@
-import { router as algorithmExecuteRouter } from './algorithm.execute.route';
-import { router as databaseRouter } from './database.algorithms.route';
 import { router as algorithmsRouter } from './algorithms.route';
 import { router as userRouter } from './users.route';
+
+import { router as singleAlgorithmRouter } from './single-algorithm.route';
 import express from 'express';
 
 const router = express.Router();
 
-// router.use('/community/algorithms', databaseRouter);
-router.use('/algorithm/execute', algorithmExecuteRouter);
+router.use('/algorithm', singleAlgorithmRouter);
 router.use('/users', userRouter);
 router.use('/algorithms', algorithmsRouter);
 
--- src/routes/single-algorithm.route.ts
+++ src/routes/single-algorithm.route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { APIResponse } from '#src/shared-types/APIResponse';
-import { VisualizationRequest } from '#src/shared-types/visualization/VisualizationRequest';
 import { algorithmExecutor } from '#src/controllers/algorithm';
 
 const router = express.Router();
 
-router.post('/', async (request, response) =>
+/*
+ *
+ * Execute Python code sent from browser
+ *
+ */
+router.post('/execute', async (request, response) =>
   algorithmExecutor.handleRequest(request, response),
 );
 
