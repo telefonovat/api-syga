b1a7edf07267e4e171c52dee3b4c66db80785f36
telefon <kheltan.engineering@gmail.com>
2026-03-04 23:55:16 +0100

    feat: add format endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -30,6 +30,45 @@ router.get(
   },
 );
 
+router.post('/v2/format', async (request, response) => {
+  try {
+    const body = request.body;
+    if (!body.code) {
+      response.status(400).send({ error: 'The request has no code' });
+    }
+
+    const code: string = body.code;
+
+    const result = await fetch('http://nginx:80/engine/v1/format', {
+      method: 'POST',
+      body: JSON.stringify({ code }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const resultBody: any = await result.json();
+
+    if (!resultBody.formatted_code) {
+      response
+        .status(500)
+        .send({ error: 'The engine would not format the code.' });
+    }
+
+    response
+      .status(200)
+      .send({ formattedCode: resultBody.formatted_code });
+  } catch (e) {
+    if (e instanceof Error) {
+      response.status(400).send({ error: e.message });
+    } else {
+      response.status(500).send({
+        error:
+          'An unknown error occured while trying to fetch algorithm',
+      });
+    }
+  }
+});
+
 router.get('/v2/ping', (_, response) =>
   response.send('Ping succeeded.'),
 );
